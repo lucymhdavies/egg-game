a540ae6fb23a399b4c094c22d2e77098c32e0bcd
Lucy Davinhart <lucy@lucymhdavies.com>
2019-04-06 23:05:55 +0100

    --phone flag when running the game, to use a a resolution approximating an iPhone XR
    Adds part of https://github.com/lucymhdavies/egg-game/issues/22 , but
    literally the only version of this that applies to me specifically.
    
    Needs a generic way of doing this, but for my purposes this is okay for now.

--- main.go
+++ main.go
@@ -7,7 +7,16 @@ import (
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/lucymhdavies/egg-game/game"
+	flag "github.com/spf13/pflag"
 )
+
+var (
+	phoneMode = flag.Bool("phone", false, "Set resolution to approximate an iPhone XR")
+)
+
+func init() {
+	flag.Parse()
+}
 
 func main() {
 	ebiten.SetRunnableInBackground(true)
@@ -21,6 +30,14 @@ func main() {
 		game.ScreenWidth = int(float64(w) / scaleFactor)
 		game.ScreenHeight = int(float64(h) / scaleFactor)
 
+	}
+
+	if *phoneMode {
+		// https://www.paintcodeapp.com/news/ultimate-guide-to-iphone-resolutions
+		game.ScreenWidth = int(414.0 / scaleFactor)
+		// Includes 100 pt to account for notch
+		// Meaured with the totally accurate "put phone against screen" technique
+		game.ScreenHeight = int((896.0 - 100.0) / scaleFactor)
 	}
 
 	g := game.NewGame()
